a1bab32e0f97618b2a97d400e9e86a3ba6a5820b
James Sharpe <james.sharpe@zenotech.com>
2017-09-07 09:15:00 +0100

    Update create credentials to use post

--- Epic_CLI.py
+++ Epic_CLI.py
@@ -99,7 +99,9 @@ def aws_get():
 @accounts.command()
 def aws_create():
     """Create AWS Credentials, if the don't already exist"""
-    get_request('/accounts/aws/create/', get_request_headers())
+    response = post_request('/accounts/aws/create/', get_request_headers())
+    for i in response:
+        print(i + ": " + response[i])
 
 
 @accounts.command('list')
